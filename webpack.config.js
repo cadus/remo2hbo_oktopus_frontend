08bada83910ad58650af8283a3ca06b8bf6c5f80
andrea <andrea@kreichgauer.com>
2018-12-19 10:54:05 +0100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+          },
+          {
+            test: /\.css$/,
+            use: [
+              { loader: "style-loader" },
+              { loader: "css-loader" }
+            ]
+          },
+          {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {}
+              }
+            ]
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['*', '.js', '.jsx']
+      },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    devServer: {
+      contentBase: './dist'
+    }
+  };

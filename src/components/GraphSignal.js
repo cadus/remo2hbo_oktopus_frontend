90efaac6c9537f916f2253d31176a585b0b47eab
andrea <andrea@kreichgauer.com>
2019-01-09 14:01:28 +0100

    refactoring

--- src/components/GraphSignal.js
+++ src/components/GraphSignal.js
@@ -1,5 +1,6 @@
 import Dygraph from 'dygraphs';
 import React, { Component } from 'react';
+import { runInThisContext } from 'vm';
 class GraphSignal extends Component {
 
     constructor(props){
@@ -14,7 +15,7 @@ class GraphSignal extends Component {
     }
 
     componentDidMount() {
-        console.log("DidMount ", this.props.bioSignalValue);
+        //console.log("DidMount ", this.props.bioSignalValue);
         this.saveSignal();
         this.timerID = setInterval(
             () => this.drawGraph(),
@@ -38,7 +39,7 @@ class GraphSignal extends Component {
     }
 
     saveSignal() {
-        console.log("Save Signal, indexpointer: ", this.indexPointer, this.props.bioSignalType,": ", this.props.bioSignalValue ); 
+        //console.log("Save Signal, indexpointer: ", this.indexPointer, this.props.bioSignalType,": ", this.props.bioSignalValue ); 
         //console.log("saveSignal ", this.props.bioSignalType);
         let x = new Date();
         let y = Number(this.props.bioSignalValue);
@@ -60,11 +61,15 @@ class GraphSignal extends Component {
                         this.props.valueRangeMin , 
                         this.props.valueRangeMax , 
                     ],
+                    axisLineColor: "white",
+                    color: "blue",
                     drawPoints: false,
+                    drawAxis: false,
                     legend: "never",
                 });
         } else {
             let dygraphContainerOffsetWidth = document.getElementById('dygraph-container').offsetWidth
+            console.log("width: ",dygraphContainerOffsetWidth);
             //display at most around 500 values depending on canvas width
             //ToDo: lost values, e.g. if last update at length 97 and next at 103 and offset =100, values 97,98,99 will be lost because array reduced to size 100
             if(this.tempData.length >= 500){
@@ -72,7 +77,7 @@ class GraphSignal extends Component {
                 //delete items from array to remain array.length at length of offsetWidth;
                 this.tempData.splice(0,valuesToBeRemoved);
             }
-            this.graph.updateOptions( { 'file' : this.tempData } );
+            this.graph.updateOptions( { 'file' : this.tempData} );
         }
     }
 }

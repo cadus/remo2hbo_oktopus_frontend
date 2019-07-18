04a3c07d6e488c354cf771e0814e04020b72ef18
Sabina Mollenhauer <s0563968@htw-berlin.de>
2019-07-18 15:11:23 +0200

    added contrast button

--- src/container/App.js
+++ src/container/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MonitorView from './MonitorView.js';
 import MenuView from './MenuView.js';
 import SettingsIcon from '../images/settings.png';
+import ContrastIcon from '../images/contrastIcon.png';
 
 
 
@@ -12,9 +13,17 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showMenu: true
+      showMenu: true,
+      isDark: true
     };
+    this.toggleContrast = this.toggleContrast.bind(this);
     this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleContrast() {
+    this.setState(prevState => ({
+      isDark: !prevState.isDark
+    }));
   }
 
   toggleMenu() {
@@ -27,8 +36,11 @@ class App extends Component {
 
       return (
         <div>
-        <img className='settings-icon' src={SettingsIcon} onClick={this.toggleMenu}/>
-        {this.state.showMenu ?  <MenuView /> : <MonitorView />}
+          <img className='contrast-icon' src={ContrastIcon} onClick={this.toggleContrast}/>
+          <img className='settings-icon' src={SettingsIcon} onClick={this.toggleMenu}/>
+          <div className={this.state.isDark ? 'dark-contrast' : ''}>
+            {this.state.showMenu ?  <MenuView /> : <MonitorView />}
+          </div>
         </div>
 
       )
